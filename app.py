73cacca9717813ff7b1feb207639e6f323ec2342
tomonaos <137622728+tomonaos@users.noreply.github.com>
2023-06-29 21:11:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def communicate():
 # ユーザーインターフェイスの構築
 st.title("AI面接官")
 #st.write("それでは面接を始めます。まずは自己紹介をお願いします。")
-if messages[-1]:
-    msg = messages[-1]
+if messages[0]:
+    msg = messages[0]
 st.write(msg["content"])
 
 user_input = st.text_input("INPUT:", key="user_input", on_change=communicate)

71379c59f6368534b2dfc2cc1361fafe48f092cc
tomonaos <137622728+tomonaos@users.noreply.github.com>
2023-07-01 14:43:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀な面接官です。就活生の回答に対して適切な質問をしてください。5回質問をしたら面接を終えてください。"}
+        {"role": "system", "content": "あなたは優秀な面接官です。就活生の回答に対して適切な質問をしてください。},
+        {"role": "assistant", "content": "あなたは優秀な面接官です。就活生の回答に対して適切な質問をしてください。5回質問をしたら面接を終えてください。"}
         ]
 
 # チャットボットとやりとりする関数

d53eaeb0ec0d785ec987591cf92186264734b090
tomonaos <137622728+tomonaos@users.noreply.github.com>
2023-07-01 15:01:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def communicate():
     bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
-    bot_message_content = bot_message["content"]
+    #bot_message_content = bot_message["content"]
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
